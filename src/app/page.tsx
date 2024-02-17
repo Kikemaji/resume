2d975b6fcf5e254c806274b7e5b715a705b5e173
Kike Márquez <kikemarquezjimenez@gmail.com>
2024-02-17 17:13:58 +0100

    Quality section mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { FaCode } from 'react-icons/fa6';
+import { FaHeart } from 'react-icons/fa6';
 
 import { Navbar } from '@/components/ui/Navbar';
 import Image from 'next/image';
@@ -34,12 +35,35 @@ export default function Home() {
             />
           </div>
         </section>
-        <div className="my-4 opacity-80">
-          <p>Calidad asegurada por:</p>
-          <div className="flex gap-2">
-            <div>ESLint</div>
-            <div>Sonnar</div>
-            <div>Puede que tú si ves el código</div>
+        <div className="my-4 ">
+          <p className="opacity-50">Calidad asegurada por:</p>
+          <div className="mt-1 flex items-end gap-2">
+            <div className="flex flex-col gap-1">
+              <Image
+                className=""
+                alt="ESLint icon"
+                src="/eslint.svg"
+                width={24}
+                height={24}
+              />
+              <span className="text-xs">ESLint</span>
+            </div>
+            <div
+              className="
+            text-center text-xs"
+            >
+              <p>Principios</p>SOLID
+            </div>
+            {/* <div>¿Sonnar?</div> */}
+            <div className="text-xs">
+              <p>¿Por ti?</p>
+              <p className="flex items-center gap-2">
+                <a href="" className="underline">
+                  Dale amor al repo
+                </a>
+                <FaHeart className="animate-heart-pulse text-red-500" />
+              </p>
+            </div>
           </div>
         </div>
         <section className="flex flex-col">
@@ -61,7 +85,7 @@ export default function Home() {
             <div>Next.js</div>
             <div>React.js</div>
             <div>Tailwind.js</div>
-            <div>Typescript..</div>{' '}
+            <div>Typescript..</div>
           </div>
         </section>
         <section>
