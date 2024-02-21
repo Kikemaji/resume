55b2774ea00552d51b28cb69e89d02beb2917655
Kike Márquez <kikemarquezjimenez@gmail.com>
2024-02-21 19:45:52 +0100

    Ideal job section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import {
   FaPlus,
   FaMicrophone,
   FaCamera,
+  FaCheck,
+  FaArrowDown,
 } from 'react-icons/fa6';
 import { RiTeamFill } from 'react-icons/ri';
 import { GiCrystalGrowth } from 'react-icons/gi';
@@ -15,6 +17,7 @@ import MapChart from '@/components/maps/MapChart';
 import { FaUserCircle } from 'react-icons/fa';
 import Dialog from '@/components/ui/Dialog/Dialog';
 import Accordion from '@/components/ui/Accordion/Accordion';
+import clsx from 'clsx';
 
 export default function Home() {
   const comienzoAprox = new Date(2022, 7, 12).getTime();
@@ -27,6 +30,28 @@ export default function Home() {
     (diferenciaEnMilisegundos % (365.25 * 24 * 60 * 60 * 1000)) /
       (30.44 * 24 * 60 * 60 * 1000)
   );
+
+  const trabajoIdealContent = [
+    'Didáctico',
+    'Entretenido',
+    'Con buen ambiente',
+    'Equilibrio vida / trabajo',
+    'Buen sueldo y posibilidad de subirlo',
+  ];
+
+  const trabajoIdealExamples = [
+    'Reviews de código',
+    'Comunicación',
+    'Buenas prácticas',
+    'Diferentes frameworks/proyectos',
+    'Testing',
+    'Remoto',
+    'Sin horas extra',
+    'Agile',
+    'Buenos compañeros',
+    'Aprender backend',
+    'Revisiones salariales',
+  ];
   return (
     <main className="flex min-h-screen flex-col bg-background text-text ">
       <Navbar />
@@ -319,6 +344,28 @@ export default function Home() {
             <span>Hacer algo (esconder en movil)</span>
           </div>
         </section>
+        <section className="my-8 flex flex-col gap-4">
+          <h2 className="text-center text-xl font-bold">
+            ¿Cómo es mi trabajo ideal?
+          </h2>
+          <div className="flex flex-col items-center gap-4 md:flex-row">
+            <ul className="text-lg md:w-[30%] ">
+              {trabajoIdealContent.map((text) => (
+                <li key={text} className="flex items-center gap-2.5">
+                  <FaCheck />
+                  {text}
+                </li>
+              ))}
+            </ul>
+            <FaArrowDown className=" h-12 w-12 md:w-[20%] md:-rotate-90" />
+
+            <div className="flex flex-wrap justify-around gap-2 md:w-[50%]">
+              {trabajoIdealExamples.map((text) => (
+                <Label key={text} text={text} />
+              ))}
+            </div>
+          </div>
+        </section>
         <section className="my-4 flex flex-col gap-4">
           <h2 className="text-center text-xl font-bold">
             Preguntas frecuentes
@@ -329,3 +376,19 @@ export default function Home() {
     </main>
   );
 }
+
+const Label = ({ text }: { text: string }) => {
+  const randomColor = Math.floor(Math.random() * 3);
+  return (
+    <div
+      className={clsx(
+        'rounded-md p-1 shadow-md',
+        randomColor === 0 && 'shadow-green-600',
+        randomColor === 1 && 'shadow-pink-600',
+        randomColor === 2 && 'shadow-blue-600'
+      )}
+    >
+      {text}
+    </div>
+  );
+};
